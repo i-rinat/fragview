49878a7ea43dd4d546f2e9ef9502a8fb06fa42e7
Rinat <ibragimovrinat@mail.ru>
2011-08-31 21:43:53 +0400

    now it can create db and table in it

--- fragdb.cpp
+++ fragdb.cpp
@@ -7,14 +7,38 @@
 
 #include "clusters.h"
 #include <sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *sql_create_tables =
+"DROP TABLE IF EXISTS items; "
+"CREATE TABLE items( "
+"id INTEGER PRIMARY KEY, "
+"name TEXT, "
+"fragments INTEGER "
+"); "
+"CREATE INDEX name_idx ON items (name); "
+"CREATE INDEX frag_idx ON items (fragments); ";
 
 int main (int argc, char *argv[]) {
 
     sqlite3 *db;
     int res;
+    char *errmsg;
 
-    res = sqlite3_open("fragmentator.db", &db);
+    res = sqlite3_open ("fragmentator.db", &db);
+    if (SQLITE_OK != res) {
+        fprintf (stderr, "error: can't create/open database\n");
+        exit (1);
+    }
 
-    res = sqlite3_close(db);
+    res = sqlite3_exec (db, sql_create_tables, NULL, NULL, &errmsg);
+    if (SQLITE_OK != res) {
+        fprintf (stderr, "error: can't create table\n");
+        fprintf (stderr, "sqlite3 said \"%s\"", errmsg);
+        exit (1);
+    }
+
+    res = sqlite3_close (db);
 
 }
