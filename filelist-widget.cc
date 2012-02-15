a36740c3622048f51e9bca96ae4496b8aa49e588
Rinat <ibragimovrinat@mail.ru>
2012-02-15 20:33:58 +0400

    add headers to treeview

--- filelist-widget.cc
+++ filelist-widget.cc
@@ -2,7 +2,20 @@
 
 FilelistView::FilelistView ()
 {
+    liststore = Gtk::ListStore::create (columns);
+    set_model (liststore);
+    append_column ("fileid", columns.col_fileid);
+    append_column ("Fragments", columns.col_fragments);
+    append_column ("Severity", columns.col_severity);
+    append_column ("Name", columns.col_name);
+    append_column ("Dir", columns.col_dir);
 
+    set_headers_clickable (true);
+
+    for (int k = 1; k < 15; k ++) {
+        Gtk::TreeModel::Row row = *(liststore->append());
+        row[columns.col_fileid] = k;
+    }
 }
 
 FilelistView::~FilelistView ()
--- filelist-widget.h
+++ filelist-widget.h
@@ -1,8 +1,32 @@
 #include <gtkmm/treeview.h>
+#include <gtkmm/liststore.h>
 
 class FilelistView : public Gtk::TreeView
 {
+    class ModelColumns : public Gtk::TreeModelColumnRecord {
+        public:
+            ModelColumns ()
+            {
+                add (col_fileid);
+                add (col_fragments);
+                add (col_severity);
+                add (col_name);
+                add (col_dir);
+            };
+
+            Gtk::TreeModelColumn<int> col_fileid;
+            Gtk::TreeModelColumn<int> col_fragments;
+            Gtk::TreeModelColumn<double> col_severity;
+            Gtk::TreeModelColumn<Glib::ustring> col_name;
+            Gtk::TreeModelColumn<Glib::ustring> col_dir;
+    };
+
     public:
         FilelistView ();
         ~FilelistView ();
+
+    protected:
+        ModelColumns columns;
+        Glib::RefPtr<Gtk::ListStore> liststore;
+
 };
