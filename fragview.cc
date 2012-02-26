58de8a0573090b2f9d6673f42d4e742bc97481d5
Rinat <ibragimovrinat@mail.ru>
2012-02-26 19:28:31 +0400

    enlarge your menus

--- fragview.cc
+++ fragview.cc
@@ -25,10 +25,25 @@ class GraphWindow : public Gtk::Window {
         std::string initial_dir;
         Glib::RefPtr<Gtk::ActionGroup> action_group_ref;
         Glib::RefPtr<Gtk::UIManager> ui_manager_ref;
+
+        void on_action_view_most_fragmented (void);
+        void on_action_main_quit (void);
 };
 
+void
+GraphWindow::on_action_view_most_fragmented (void)
+{
+    std::cout << "Ha!" << std::endl;
+}
+
+void
+GraphWindow::on_action_main_quit (void)
+{
+    this->hide ();
+}
+
 GraphWindow::GraphWindow (const std::string& initial_dir) {
-    set_title ("graph");
+    set_title ("fragview");
     set_default_size (800, 560);
     fragmap.attach_clusters (cl);
     fragmap.attach_filelist_widget (filelist);
@@ -39,7 +54,32 @@ GraphWindow::GraphWindow (const std::string& initial_dir) {
     // set up menus
     action_group_ref = Gtk::ActionGroup::create ();
 
-    // TODO: WIP
+    action_group_ref->add (Gtk::Action::create ("MenuMain", "fragview"));
+    action_group_ref->add (Gtk::Action::create ("Quit", "Quit"),
+        sigc::mem_fun (*this, &GraphWindow::on_action_main_quit));
+
+    action_group_ref->add (Gtk::Action::create ("MenuView", "View"));
+    action_group_ref->add (Gtk::Action::create ("ViewMostFragmentedFiles", "Most fragmented files"),
+        sigc::mem_fun (*this, &GraphWindow::on_action_view_most_fragmented));
+
+    ui_manager_ref = Gtk::UIManager::create ();
+    ui_manager_ref->insert_action_group (action_group_ref);
+    add_accel_group (ui_manager_ref->get_accel_group ());
+
+    Glib::ustring ui_info =
+        "<ui>"
+        "  <menubar name='MenuBar'>"
+        "    <menu action='MenuMain'>"
+        "      <menuitem action='Quit' />"
+        "    </menu>"
+        "    <menu action='MenuView'>"
+        "      <menuitem action='ViewMostFragmentedFiles' />"
+        "    </menu>"
+        "  </menubar>"
+        "</ui>";
+
+    ui_manager_ref->add_ui_from_string (ui_info);
+    Gtk::Widget *menubar = ui_manager_ref->get_widget ("/MenuBar");
 
     Gtk::ScrolledWindow *scrolled_window = Gtk::manage (new Gtk::ScrolledWindow);
     scrolled_window->add (filelist);
@@ -49,9 +89,8 @@ GraphWindow::GraphWindow (const std::string& initial_dir) {
     vpaned->pack2 (*scrolled_window, true, false);
 
     Gtk::VBox *vbox = Gtk::manage (new Gtk::VBox);
+    vbox->pack_start (*menubar, false, false);
     vbox->pack_start (*vpaned, true, true);
-    Gtk::Button *btn = Gtk::manage (new Gtk::Button ("Button that do nothing"));
-    vbox->pack_start (*btn, false, false);
 
     add (*vbox);
     show_all_children ();
