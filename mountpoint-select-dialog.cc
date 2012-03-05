b143fd9981d548da6db8865ad8ea3d30275ba7ab
Rinat <ibragimovrinat@mail.ru>
2012-03-06 00:34:10 +0400

    select mountpoint: added treemodel

--- mountpoint-select-dialog.cc
+++ mountpoint-select-dialog.cc
@@ -15,6 +15,17 @@ MountpointSelectDialog::MountpointSelectDialog (void)
     Gtk::Button *button_ok = add_button (Gtk::Stock::OK, Gtk::RESPONSE_OK);
     get_vbox ()->pack_start (tv, Gtk::PACK_EXPAND_WIDGET, 10);
     show_all_children ();
+
+    // prepare table
+    liststore = Gtk::ListStore::create (columns);
+    tv.set_model (liststore);
+    tv.append_column ("id", columns.id);
+    tv.append_column ("Mount point", columns.mountpoint);
+    tv.append_column ("Size", columns.size);
+    tv.append_column ("Used", columns.used);
+    tv.append_column ("Available", columns.available);
+    tv.append_column ("Type", columns.type);
+    tv.append_column ("Available", columns.available_percentage);
 }
 
 MountpointSelectDialog::~MountpointSelectDialog (void)
--- mountpoint-select-dialog.h
+++ mountpoint-select-dialog.h
@@ -4,6 +4,7 @@
 
 #include <gtkmm/dialog.h>
 #include <gtkmm/treeview.h>
+#include <gtkmm/liststore.h>
 #include <stdint.h>
 
 class MountpointSelectDialog : public Gtk::Dialog
@@ -13,30 +14,32 @@ class MountpointSelectDialog : public Gtk::Dialog
         ~MountpointSelectDialog (void);
 
     protected:
-        Gtk::TreeView   tv;
-
         class ModelColumns : public Gtk::TreeModelColumnRecord
         {
         public:
             ModelColumns ()
             {
-                add (col_id);
-                add (col_mountpoint);
-                add (col_size);
-                add (col_free);
-                add (col_available);
-                add (col_type);
-                add (col_available_percentage);
+                add (id);
+                add (mountpoint);
+                add (size);
+                add (used);
+                add (available);
+                add (type);
+                add (available_percentage);
             }
 
-            Gtk::TreeModelColumn<int> col_id;
-            Gtk::TreeModelColumn<Glib::ustring> col_mountpoint;
-            Gtk::TreeModelColumn<uint64_t> col_size;
-            Gtk::TreeModelColumn<uint64_t> col_free;
-            Gtk::TreeModelColumn<uint64_t> col_available;
-            Gtk::TreeModelColumn<Glib::ustring> col_type;
-            Gtk::TreeModelColumn<int> col_available_percentage;
+            Gtk::TreeModelColumn<int> id;
+            Gtk::TreeModelColumn<Glib::ustring> mountpoint;
+            Gtk::TreeModelColumn<uint64_t> size;
+            Gtk::TreeModelColumn<uint64_t> used;
+            Gtk::TreeModelColumn<uint64_t> available;
+            Gtk::TreeModelColumn<Glib::ustring> type;
+            Gtk::TreeModelColumn<int> available_percentage;
         };
+
+        ModelColumns columns;
+        Gtk::TreeView   tv;
+        Glib::RefPtr<Gtk::ListStore> liststore;
 };
 
 #endif
